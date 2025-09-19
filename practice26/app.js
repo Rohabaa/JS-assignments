ac71f476c40bff14b249d95a387dc813f921ec01
Rohabaa <rohabak28@gmail.com>
2025-09-19 18:00:22 +0500

    prac

---
+++ practice26/app.js
@@ -0,0 +1,69 @@
+// var pass = "abcdefgh99887654343ijklmnopqr1234stuvwxyzA#$%^&*@!~,;?/|BCDEFGHIJKLMNOPQRS$%^@#*!TUVWXVZ84399JKCPO386758220"
+// var result = ""
+// for (var i=0 ; i<10 ;i ++){
+
+// }
+// chat gpt
+// var pass = "abcdefgh99887654343ijklmnopqr1234stuvwxyzA#$%^&*@!~,;?/|BCDEFGHIJKLMNOPQRS$%^@#*!TUVWXVZ84399JKCPO386758220";
+// var result = "";
+
+// // Loop 10 times
+// for (var i = 0; i < 10; i++) {
+//   // Random index choose karo
+//   var randomIndex = Math.floor(Math.random() * pass.length);
+
+//   // us index ka character result me add karo
+//   result += pass[randomIndex];
+// }
+
+// console.log("Your Random Password: " + result);
+
+// date chap31
+// var date = new Date()
+// console.log(date);
+// date = date.toString()
+// date = date.slice(0,16)
+// console.log(date);
+
+//  var date =  new Date()
+//  var day = date.getDay()
+//  console.log(day );
+ 
+// ramzan ki date
+// var today = new Date()
+// // console.log(today);
+// var ramzan = new Date ("2026-02-17")
+// console.log(ramzan);
+// // milisec ka diff
+// let diffInMs = ramzan - today;
+// console.log(diffInMs);
+
+// // Din ka difference (1 din = 1000 ms * 60 sec * 60 min * 24 hr)
+// let diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+// console.log(diffInDays);
+// document.write("17 Feb 2026 tak " + diffInDays + " din bache hain.");
+
+  
+    // var ramzan = new Date ("17 feb 2026")
+    // var today = new Date ()
+    // var difference = ramzan - today 
+    // console.log(difference);
+    // var days = Math.floor(difference /(1000 * 60 * 60 *24) ) 
+    // console.log(days);
+    // var month = Math.floor(difference /(1000 * 60 * 60 *24*30) ) 
+    // console.log(month);
+    
+    //  age nikali
+//   // User se birth year input lo
+//   let birthYear = prompt("Apna birth year likho (e.g. 2005):");
+
+//   // Current year nikal lo
+//   let currentYear = new Date().getFullYear();
+
+//   // Age calculate karo
+//   let age = currentYear - birthYear;
+
+//   document.write("Aap ki age hai: " + age + " saal.");
+    
+    
+    
