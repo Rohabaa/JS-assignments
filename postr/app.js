78ca086d89b45c20c84e5d54837b1776b808c12f
Amberfatima4 <emberfatima24@gmail.com>
2025-10-30 18:03:44 +0500

    postrupdated

--- postr/app.js
+++ postr/app.js
@@ -1,29 +1,43 @@
-function createaccount(){
-    var fname = document.getElementById("fname").value
-        var lname = document.getElementById("lname").value
-        var pass = document.getElementById("pass").value
-        var email = document.getElementById("email").value
-        var phone = document.getElementById("mob").value
-        var city = document.getElementById("city").value
-        console.log(fname,lname);
-        
-if(fname && lname && email && pass && phone && city){
-    Swal.fire({
-  position: "top-end",
-  icon: "success",
-  title: "Your work has been saved",
-  showConfirmButton: false,
-  timer: 1500
-});
-document.getElementById("signupBox").style.display = "none";
-document.getElementById("postcard").style.display="block"
+function createaccount() {
+  var fname = document.getElementById("fname").value;
+  
+  
+  var lname = document.getElementById("lname").value;
+  var pass = document.getElementById("pass").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("mob").value;
+  var city = document.getElementById("city").value;
+  console.log(fname, lname);
 
-}else{
+  if (fname && lname && email && pass && phone && city) {
     Swal.fire({
-  icon: "error",
-  title: "Oops...",
-  text: "Please fill all fields!",
-});
+      position: "top-end",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    document.getElementById("signupBox").style.display = "none";
+    document.getElementById("postcard").style.display = "block";
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill all fields!",
+    });
+  }
 }
-
+var display = document.getElementById("display")
+function showPostcard(){
+  var title = document.getElementById("postinput").value
+  var description = document.getElementById("about").value
+  
+display.innerHTML+=`
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+</div>
+`
 }
