0254c12d6327ccc7bc7424b2e207fe13421e2813
Amberfatima4 <emberfatima24@gmail.com>
2025-11-06 18:10:06 +0500

    postr

--- postr/app.js
+++ postr/app.js
@@ -1,7 +1,23 @@
-function createaccount() {
+ 
+ function deletepost(){
+ var card =  event.target.parentNode.parentNode.parentNode   
+ card.remove()
+ }
+ function editpost(){
+  var card =  event.target.parentNode.parentNode.parentNode
+
+  var  title = card.childNodes[5].childNodes[1].innerHTML
+  var  description = card.childNodes[5].childNodes[3].innerHTML
+  document.getElementById("postinput").value = title
+  document.getElementById("about").value = description
+  card.remove()
+  // console.log(event.target.parentNode.parentNode.parentNode  );
+  console.log(title);
+ }
+ 
+ function createaccount() {
   var fname = document.getElementById("fname").value;
-  
-  
+
   var lname = document.getElementById("lname").value;
   var pass = document.getElementById("pass").value;
   var email = document.getElementById("email").value;
@@ -19,7 +35,7 @@ function createaccount() {
     });
     document.getElementById("signupBox").style.display = "none";
     document.getElementById("postcard").style.display = "block";
- document.body.classList.remove("active");
+    document.body.classList.remove("active");
   } else {
     Swal.fire({
       icon: "error",
@@ -28,37 +44,37 @@ function createaccount() {
     });
   }
 }
-var display = document.getElementById("display")
-function showPostcard(){
-    var email = document.getElementById("email").value;
-     let now = new Date();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let seconds = now.getSeconds();
+var display = document.getElementById("display");
+function showPostcard() {
+  var email = document.getElementById("email").value;
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
 
-  var title = document.getElementById("postinput").value
-  var description = document.getElementById("about").value
-  
- 
-  
-display.innerHTML+=`
+  var title = document.getElementById("postinput").value;
+  var description = document.getElementById("about").value;
+
+  display.innerHTML += `
  <div class="card post2 ">
- <div class="class-header header1">${email}
-<div> ${hours}:${minutes}:${seconds}</div>
- </div>
+ <div class="class-header header1">${email} </div>
+
+<div class ="date"> ${hours}:${minutes}:${seconds}</div>
 
   <div class="card-body">
     <h5 class="card-title ">${title}</h5>
     <p class="card-text">${description}</p>
-    <div class="edit"> <button type="button" class="btn  edit1 ">Edit</button>
+    <div class="ed"> <button onclick="editpost()" type="button" class="btn  edit1 ">Edit</button> 
 
- <div class="delete"><button type="button" class="btn edit2 ">Delete</button>  </div>
+ <button onclick="deletepost()" type="button" class="btn edit2 ">Delete</button> </div>
  
-    </div>
+   
  </div>
 </div>
-`
-document.getElementById("postinput").value = ""
-document.getElementById("about").value = ""
-
+`;
+  document.getElementById("postinput").value = "";
+  document.getElementById("about").value = "";
+}
+function imagepost(src){
+   event.target.classList.add("forborder")
 }
